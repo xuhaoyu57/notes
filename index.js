97af1ee745fec5e3aea63a31b12a77b0cba34b7d
xuhaoyu57 <306648464@qq.com>
2026-06-25 15:05:43 +0800

    resolve conflict use local file

---
+++ index.js
@@ -0,0 +1,3 @@
+export const url = "https://daixzsefottmustefsuj.supabase.co/rest/v1/";
+export const key = "sb_publishable_hygvKth7LB1Xd2Ls0FQXJw_Sr_SiZL_"; // 用 publishable key
+export const img_url = "https://daixzsefottmustefsuj.supabase.co/storage/v1/object/images/";
