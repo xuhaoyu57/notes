83c786125ebfe1ac1399d251f071aaedef137341
xuhaoyu57 <306648464@qq.com>
2026-06-26 11:07:52 +0800

    主体

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ export const img_url = "https://daixzsefottmustefsuj.supabase.co/storage/v1/obje
 // export const publicUrl = "https://daixzsefottmustefsuj.supabase.co/storage/v1/object/public/images/"//获取图片接口
 //获取数据
 export async function getData(url, params = {}) {
-    const res = await fetch(API + url + '?${params.toString()}', {
+    const special = ["limit", "offset", "order", "select"];//这些跳过不添加eq，eq是筛选条件的
+    const query = Object.entries(params)
+        .map(([key, value]) => {
+            if (special.includes(key)) {
+                return `${key}=${encodeURIComponent(value)}`;
+            }
+            return `${key}=eq.${encodeURIComponent(value)}`;
+        })
+        .join("&");
+    //limit=20每页20条offset 表示跳过前面多少条记录，可以理解为从第几个下标开始取
+    const res = await fetch(API + url + '?' + query, {
         method: "GET",
         headers: {
             apikey: key,
@@ -56,7 +66,7 @@ export async function delImg(url) {
         },
     })
     console.log(res);
-    
+
     if (res.status == 200) return alert('删除成功')
     else return null
 }
